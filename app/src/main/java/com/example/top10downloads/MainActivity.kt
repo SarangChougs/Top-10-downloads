8ea5e218e3ef2428ab56ee9207c5a234e710d62c
Sarang Chougs <sarangchouguley284@gmail.com>
2019-06-08 18:45:36 +0530

    kotlin way of doing things.

--- app/src/main/java/com/example/top10downloads/MainActivity.kt
+++ app/src/main/java/com/example/top10downloads/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.io.BufferedReader
+//import java.io.BufferedReader
 import java.io.IOException
 //import java.io.InputStream
-import java.io.InputStreamReader
+//import java.io.InputStreamReader
 import java.lang.Exception
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
@@ -52,39 +52,26 @@ class MainActivity : AppCompatActivity() {
 
                 try {
                     val url = URL(urlpath)
-                    val connection :HttpURLConnection =  url.openConnection() as HttpURLConnection
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
                     val response = connection.responseCode
-                    Log.d(TAG,"downloadXML: responceCode is $response")
+                    Log.d(TAG, "downloadXML: responceCode is $response")
 
-//            val inputStream = connection.inputStream
-//            val inputStreamReader = InputStreamReader(inputStream)
-//            val reader = BufferedReader(inputStreamReader)
 
-                    // creating the reader which will read the stream of characters and store them in a buffer
-                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    //kotlin way of doing things
+                    connection.inputStream.buffered().reader().use{xmlResult.append(it.readText()) }
 
-                    //appending the characters received in the reader
-                    val inputBuffer = CharArray(500)
-                    var charRead = 0
-                    while (charRead >= 0){
-                        charRead = reader.read(inputBuffer)
-                        if(charRead > 0){
-                            xmlResult.append(String(inputBuffer,0,charRead))
-                        }
-                    }
-                    reader.close()
-
-                    Log.d(TAG,"Received ${xmlResult.length} bytes")
+                    Log.d(TAG, "Received ${xmlResult.length} bytes")
                     return xmlResult.toString()
 
-                }catch (e:MalformedURLException){
-                    Log.e(TAG,"downloadXML :invalid URL ${e.message}")
-                }catch (e:IOException){
-                    Log.e(TAG,"downloadXML :IO exception reading data : ${e.message}")
-                }catch (e:SecurityException){
-                    Log.e(TAG, "downloadXML : Security Exception , Need permission : ${e.message}")
                 }catch (e: Exception){
-                    Log.e(TAG, "downloadXML :unknown error: ${e.message}")
+                    val errorMessage:String = when (e){
+                        is MalformedURLException -> "downloadXML :invalid URL ${e.message}"
+                        is IOException -> "downloadXML :IO exception reading data : ${e.message}"
+                        is SecurityException -> { e.printStackTrace()
+                            "downloadXML : Security Exception , Need permission : ${e.message}"}
+                        else -> "downloadXML :unknown error: ${e.message}"
+                    }
+                    Log.d(TAG,errorMessage)
                 }
 
                 return "" // if it get till here means there is some error hence return a empty string.
